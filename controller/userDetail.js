0cd05e3b4fc5f0d7ac3f394dd9fcdc102e107509
Mohankumar-21 <112240836+Mohankumar-21@users.noreply.github.com>
2024-05-29 07:36:56 +0530

    Update userDetail.js

--- controller/userDetail.js
+++ controller/userDetail.js
@@ -1,25 +1,33 @@
 const getUserDetail = require("../middleware/getUserDetail");
 
-const userDetails = async(req,res)=>
-{
-   try
-   {
-      const token = req.cookies.token || "" ;
-      const user = await getUserDetail(token);
-      return res.status(200).json({
-        message : "user details",
-        data : user
-      })
+const userDetails = async (req, res) => {
+  try {
+    // Check for token in cookies, Authorization header, and query parameters
+    const token = req.cookies.token || 
+                  req.headers.authorization && req.headers.authorization.split(' ')[1] || 
+                  req.query.token || 
+                  "";
 
-   }
-   catch(error)
-   {
+    if (!token) {
+      return res.status(401).json({
+        message: "No token provided",
+        error: true,
+      });
+    }
+
+    const user = await getUserDetail(token);
+
+    return res.status(200).json({
+      message: "user details",
+      data: user
+    });
+
+  } catch (error) {
     return res.status(500).json({
-        message : error.message || error,
-        error : true,
-    })
-   }
+      message: error.message || error,
+      error: true,
+    });
+  }
 }
-
 
 module.exports = userDetails;
